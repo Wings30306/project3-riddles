472ecac580fc0532ac99d945fd521c4014c109bd
Joke Heyndels <jo_hannah@outlook.com>
2018-08-28 19:33:20 +0200

    lowercase had a bug. Bug is now fixed.

--- app.py
+++ app.py
@@ -77,7 +77,8 @@ def play_game():
             print(riddle["riddle"] + "\n")
             total_questions += 1
             #user's answer is registered in the guess variable
-            guess = input("Your answer: ").lower
+            guess = input("Your answer: ")
+            guess = guess.lower()
             answer = riddle["answer"]
             #this if statement checks whether the answer keyword from the file is in the guess
             #this prevents answers from being read as wrong due to an article or a verbose phrasing
