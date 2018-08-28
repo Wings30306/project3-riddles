2ac2ebb313a0c8bd4ce7679fa8d84bf5c640b028
Joke Heyndels <jo_hannah@outlook.com>
2018-08-28 16:01:07 +0200

    Register function added to app.

--- app.py
+++ app.py
@@ -16,11 +16,29 @@ def menu():
         if option == "1":
             print("You have chosen Sign in")
         elif option == "2":
-            print("You have chosen Register")
+            register()
         else: 
             break 
         print("")
 
+
+"""
+If option 2 is selected in the menu, this function will allow the user to register.
+If the username is already in use, the user will be notified.
+"""
+def register():
+    user = input("Please choose a username\n>")
+    active_users = {}
+
+    with open("data/users.txt", "r") as file:
+        active_users = file.read().splitlines()
+        if user in active_users:
+            print("sorry, this username is taken. Please choose a different username.")
+        else:
+            print("You are now registered. Please sign in to go to the game.")
+            file = open("data/users.txt", "a")
+            file.write(user + "\n")
+
+
+
 menu()
-
-
