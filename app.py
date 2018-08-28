763c76aaaef4ad2cc35c4986fdc2a1cc567393ec
Joke Heyndels <jo_hannah@outlook.com>
2018-08-28 18:47:16 +0200

    added sign-in function

--- app.py
+++ app.py
@@ -14,12 +14,31 @@ def menu():
     while True:
         option = show_menu()
         if option == "1":
-            print("You have chosen Sign in")
+            sign_in()
         elif option == "2":
             register()
         else: 
             break 
         print("")
+
+
+"""
+If option 1 is selected in the menu, this function will allow the user to sign in.
+If the username is not registered, the user will be asked to register.
+"""
+def sign_in():
+    username = input("Your Username:\n")
+    active_users = {}
+
+    with open("data/users.txt", "r") as file:
+        active_users = file.read().splitlines()
+        if username in active_users:
+            print("Answer riddles")
+        else:
+            print("sorry, this username is incorrect. New user? Please register.")
+
+    number_of_members = len(active_users)
+    print("{0} active players right now".format(number_of_members))
 
 
 """
@@ -34,10 +53,10 @@ def register():
         active_users = file.read().splitlines()
         if user in active_users:
             print("sorry, this username is taken. Please choose a different username.")
-        else:
-            print("You are now registered. Please sign in to go to the game.")
+        else:            
             file = open("data/users.txt", "a")
             file.write(user + "\n")
+            print("You are now registered. Please sign in to continue.") 
 
 
 
