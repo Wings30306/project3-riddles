d7fe603987737598b1425c16979e434835e569d7
Joke Heyndels <jo_hannah@outlook.com>
2018-09-01 17:47:16 +0200

    register function also works

--- app.py
+++ app.py
@@ -12,20 +12,20 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/sign_in", methods=["POST"])
 def sign_in():
     name = request.form["username"]
     with open("data/users.txt", "r") as file:
         active_users = file.read().splitlines()
         if name in active_users:
             render_template("riddles.html", username=name)
-            return redirect(url_for("riddles"))
+            return redirect(url_for("riddles"), name)
         else:
             signin_message = "Sorry, this username is incorrect. New user? Please register."
             return render_template("index.html", signin_message=signin_message)
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/register", methods=["POST"])
 def register():
     name = request.form["new_user"]
     with open("data/users.txt", "r") as file:
@@ -39,8 +39,8 @@ def register():
         return render_template("index.html", register_message=register_message)
 
 
-@app.route("/riddles", methods=["GET", "POST"])
-def riddles():
+@app.route("/riddles")
+def riddles():  
     score = "X"
     total_questions = "Y"
     return render_template("riddles.html", score=score, total_questions=total_questions)
