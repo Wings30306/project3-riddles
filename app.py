569fc913947b91aa2f884153b46c4ea57d918c1d
Joke Heyndels <jo_hannah@outlook.com>
2018-09-12 14:17:06 +0200

    passing score via URL, redirects work

--- app.py
+++ app.py
@@ -16,10 +16,11 @@ def index():
 def sign_in():
     user = request.form["username"]
     with open("data/users.txt", "r") as file:
+        score = 0
         total_questions = 1
         active_users = file.read().splitlines()
         if user in active_users:
-            return redirect(f"/riddles/{user}/{total_questions}")
+            return redirect(f"/riddles/{user}/{score}/{total_questions}")
         else:
             signin_message = "Sorry, this user is incorrect. New user? Please register."
             return render_template("index.html", signin_message=signin_message)
@@ -72,41 +73,43 @@ def answers(username):
 """
 
 
-@app.route("/riddles/<username>/<total_questions>", methods=["GET", "POST"])
-def show_riddles(username, total_questions):
+@app.route("/riddles/<username>/<score>/<total_questions>", methods=["GET", "POST"])
+def show_riddles(username, total_questions, score):
     user = username
-    score = 0
+    new_score = int(score)
     if request.method == "POST":
         guess = request.form.get("guess")
         guess = guess.lower()
         answer = open_riddles()[2]
         if answer in guess:
-            score += 1
-            return redirect(f"/rightanswer/{user}/{total_questions}")
+            new_score += 1
+            return redirect(f"/rightanswer/{user}/{new_score}/{total_questions}")
         else:
-            return redirect(f"/wronganswer/{user}/{total_questions}")
+            return redirect(f"/wronganswer/{user}/{new_score}/{total_questions}")
     riddle = open_riddles()[0]
-    return render_template("riddles.html", riddle=riddle, username=user, total_questions=total_questions)
+    return render_template("riddles.html", riddle=riddle, username=user, total_questions=total_questions, score=score)
 
 
-@app.route("/rightanswer/<username>/<total_questions>", methods=["GET", "POST"])
-def result_correct(username, total_questions):
+@app.route("/rightanswer/<username>/<score>/<total_questions>", methods=["GET", "POST"])
+def result_correct(username, total_questions, score):
     user = username
+    score = score
     if request.method == "POST":
-        total_questions = int(total_questions) 
-        total_questions += 1
-        return redirect(f"/riddles/{user}/{total_questions}")
-    return render_template("rightanswer.html", username=user)
+        new_total_questions = int(total_questions) 
+        new_total_questions += 1
+        return redirect(f"/riddles/{user}/{score}/{new_total_questions}")
+    return render_template("rightanswer.html", username=user, total_questions=total_questions, score=score)
 
 
-@app.route("/wronganswer/<username>/<total_questions>", methods=["GET", "POST"])
-def result_wrong(username, total_questions):
+@app.route("/wronganswer/<username>/<score>/<total_questions>", methods=["GET", "POST"])
+def result_wrong(username, total_questions, score):
     user = username
+    score = score
     if request.method == "POST":
-        total_questions = int(total_questions) 
-        total_questions += 1
-        return redirect(f"/riddles/{user}/{total_questions}")
-    return render_template("wronganswer.html", username=user)
+        new_total_questions = int(total_questions) 
+        new_total_questions += 1
+        return redirect(f"/riddles/{user}/{score}/{new_total_questions}")
+    return render_template("wronganswer.html", username=user, total_questions=total_questions, score=score)
     
 
 if __name__ == '__main__':
